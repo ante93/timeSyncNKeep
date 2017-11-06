eaa2bfa1e7b8c668d4c9feac63ea3959fac3d580
ante93 <anteklarin2@gmail.com>
2017-11-06 13:24:14 +0100

    v1 of library

---
+++ timeSyncNKeep.cpp
@@ -0,0 +1,30 @@
+#include "timeSyncNKeep.h"
+
+timeSyncNKeep:timeSyncNKeep(String server, float timeZone, int summerTime){ //creates timeSyncNKeep object
+	//Time zone in floating point; second parameter: 1 for European summer time; 2 for US daylight saving time
+	NTPtime NTPserver(server);
+	this.timeZone = timeZone;
+	this.summerTime = summerTime;
+	rtcObject.Begin();
+}
+
+strDateTime timeSyncNKeep:getTimeFromNTP(){ //get time from ntp server
+		return NTPserver.getNTPtime(timeZone, summerTime);
+}
+
+RtcDateTime timeSyncNKeep:NTPtoRTC(NPTtime time){ //converts ntc format to rtc
+		return RtcDateTime(time.year,time.month,time.day,time.hour,time.minute,time.second);
+}
+
+void timeSyncNKeep:syncRTC(){ //sync rtc with time from NTP server
+		strDateTime time =NTPserver.getNTPtime(timeZone, summerTime)
+		rtcObject.SetDateTime(time);
+}
+
+void timeSyncNKeep:setTimeOnRTC(RTCtime time){ //manually set time on rtc
+		rtcObject.SetDateTime(time);
+}
+
+RtcDateTime timeSyncNKeep:getTimeFromRTC(){ // returns time currently on RTC
+		return rtcObject.GetDateTime();
+}
---
+++ timeSyncNKeep.h
@@ -0,0 +1,37 @@
+/*
+
+ timeSyncNKeep library combines NTPtimesESP and RtcDS3231 libraries to provide
+ universal solution to keeping time and syncing with NTP servers for application
+ that require robust time managment solutions
+ Author: Ante Klarin
+ email: anteklarin2@gmail.com
+
+*/
+
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include "NTPtimeESP.h"
+#include "RtcDateTime.h"
+#include "RtcDS1307.h"
+
+class timeSyncNKeep(){
+	public:
+		timeSyncNKeep(String server, float timeZone, int summerTime);
+		RtcDS3231<TwoWire> rtcObject(Wire);
+		strDateTime NPTtime;
+		RtcDateTime RTCtime;
+		float timeZone;
+		int summerTime;
+	
+	private:
+	
+		timeSyncNKeep(String server, float timeZone, int summerTime);
+		strDateTime getTimeFromNTP();
+		RtcDateTime NTPtoRTC(NPTtime time);
+		void syncRTC();
+		void setTimeOnRTC(RTCtime time);
+		RtcDateTime getTimeFromRTC();
+
+}
